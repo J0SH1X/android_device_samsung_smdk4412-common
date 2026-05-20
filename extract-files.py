a4f9139c4e541d50b43c2a0c966fa7d524db53a9
J0SH1X <aljoshua.hell@gmail.com>
2026-05-20 08:12:20 +0000

    s2-common: switch to python based extract-files

---
+++ extract-files.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_proto_3_9_1,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/samsung/galaxys2-common',
+]
+
+blob_fixups: blob_fixups_user_type = {
+
+
+}  # fmt: skip
+
+
+
+module = ExtractUtilsModule(
+    'galaxys2-common',
+    'samsung',
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
