dda55104e68857915d4faec85844db704cf56822
J0SH1X <aljoshua.hell@gmail.com>
2026-05-30 13:20:46 +0000

    smdk4412-common: camera: drop libcamera_client

--- camera/Android.mk
+++ camera/Android.mk
@@ -29,11 +29,14 @@ LOCAL_C_INCLUDES := \
 	system/media/camera/include \
 	hardware/samsung/exynos4/hal/include
 
-LOCAL_SHARED_LIBRARIES := libutils libcutils liblog libcamera_client libhardware libs5pjpeg
+LOCAL_SHARED_LIBRARIES := libutils libcutils liblog libhardware libs5pjpeg
+LOCAL_STATIC_LIBRARIES := android.hardware.camera.common@1.0-helper
+
 LOCAL_PRELINK_MODULE := false
 
 LOCAL_MODULE := camera.$(TARGET_BOOTLOADER_BOARD_NAME)
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
